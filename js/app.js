694594bcded89f762985f86cfbf3572134ef85e8
Edward <edward6039@gmail.com>
2017-12-09 16:45:49 -0400

    correccion inicializa

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var Calculadora = (function() {
 
-  var initialize = function() {
+  function inicializa() {
 
   };
+  return {
+    init: inicializa
+  }
+
 })();
+
+Calculadora.init();
+/*document.getElementById('0').style="width:50%;";
+
+document.getElementById('0').onclick=reducirImagen;*/
